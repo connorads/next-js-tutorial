fa7636418978d16e6e011919501181c3a0c26c6c
Connor Adams <10026538+connorads@users.noreply.github.com>
2019-10-26 15:28:05 +0100

    https://nextjs.org/learn/excel/amp

---
+++ E4-create-amp-pages/pages/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { useAmp } from 'next/amp';
+
+export const config = { amp: 'hybrid' };
+
+export default function Index(props) {
+  const isAmp = useAmp();
+  return <p>Welcome to the {isAmp ? 'AMP' : 'normal'} version of the Index page!!</p>;
+}
